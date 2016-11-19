4732381a7e24bde412787254f9d81b69e606ed32
Scott Blinch <scott@tbkcreative.com>
2016-11-19 08:40:16 -0500

    maps, defer iframes, css tweak

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+function loadIframes() {
+    var deferredIframes = document.getElementsByTagName('iframe');
 
+    for (var i = 0; i < deferredIframes.length; i++) {
+        if (deferredIframes[i].getAttribute('data-src')) {
+            deferredIframes[i].setAttribute('src', deferredIframes[i].getAttribute('data-src'));
+        }
+    }
+}
+
+window.onload = loadIframes();
